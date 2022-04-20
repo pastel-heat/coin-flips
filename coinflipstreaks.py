e2236335b2fbb7658429b8654b420439a520ca91
pastel-heat <92654513+pastel-heat@users.noreply.github.com>
2022-04-20 17:27:17 -0500

    Add files via upload

---
+++ coinflipstreaks.py
@@ -0,0 +1,54 @@
+import random
+
+# function for repeating experiment 10k times
+def repeat_experiment():
+    successes = 0 # success counter - will increase if any given experiment contains a streak of 6 consecutive flips
+
+    # run experiment 10k times, if flip coins returns true, add 1 to success counter
+    for n in range(10000):
+        if flip_coins():
+            successes += 1
+    
+    # print final calculation on what percentage of experiments contained a streak of 6
+    print(f"A streak of 6 or more appeared in {successes/10000*100:.2f}% of 10,000 experiments")
+
+# function for doing the experiment itself
+def flip_coins():
+    coin_flips = []
+    
+    # flip 100 random coins(generate random int between 0, 1), and add them to a list
+    for n in range(100):
+        coin_flips.append(random.randint(0, 1))
+    
+    # runs streak checker function, and returns true or false to repeat_experiment, depending on result of streak checker
+    return streak_checker(coin_flips)
+
+# streak checker function. this is where I think my issue might be, but I'm not 100% certain
+# checks for a streak of 6 in the list of 100 flips, and returns True if it finds one.
+def streak_checker(coin_flips):
+    # initialize streak at 1 - since if you flip a coin, no matter the result, you have a streak of 1.
+    streak = 1
+
+    # start iteration on index 1 of list, since starting on index 0 will cause a crash as program compares index 0 with index -1
+    for flip in coin_flips[1:]:
+
+        # compare most recent flip with previous flip, if they are the same, increase streak counter by 1
+        if coin_flips[flip] == coin_flips[flip - 1]:
+            streak += 1
+
+        # if not the same, reset streak counter back to 1
+        else:
+            streak = 1
+        
+
+        # if streak reaches 6, return True back to flip coins function, which then passes up to repeat_experiment
+        if streak == 6:
+            return True
+    
+    # if all flips are checked and no streak of 6 has been reached, return false
+    return False
+
+# python main guard, just learned about this recently and the use case for it. as far as i understand it is encouraged to use 
+# this, so please let me know if this looks implimented correctly to you!
+if __name__ == '__main__':
+    repeat_experiment()
